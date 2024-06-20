be9e21e8e0152d5f0a08d0852db9995ce99edb29
renaldi <renaldiiii@outlook.com>
2024-06-20 09:58:22 +0700

    delete content

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "PT. Angel Products",
-  description: "PT. Angel Products",
+  description:
+    "PT Angels Products is a company operating in the refined crystal sugar industry located in Banten province.",
 };
 
 export default function RootLayout({
@@ -16,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.jpg" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
